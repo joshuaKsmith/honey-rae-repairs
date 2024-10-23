9876643978c29b811bdae3528537ad82eff65697
Joshua Smith <mtgmirage@protonmail.com>
2024-10-23 14:28:54 -0500

    added welcome components and welcome.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,24 @@ import "./App.css"
 import { CustomerList } from "./components/customers/customersList"
 import { TicketList } from "./components/tickets/TicketList"
 import { EmployeeList } from "./components/employees/EmployeesList"
-import { Route, Routes } from "react-router-dom"
+import { Outlet, Route, Routes } from "react-router-dom"
+import { NavBar } from "./components/nav/NavBar"
 
 export const App = () => {
 	return <Routes>
-		<Route path="/tickets" element={<TicketList />} />
+		<Route 
+			path="/"
+			element={
+				<>
+					<NavBar />
+					<Outlet />
+				</>
+			}
+		>
+			<Route path="tickets" element={<TicketList />} />
+			<Route path="employees" element={<EmployeeList />} />
+			<Route path="customers" element={<CustomerList />} />
+
+		</Route>
 	</Routes>
 }
